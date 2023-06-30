c31fda7c3f988010cc58c2e49a15f3a871098647
mano araujo <manoaraujo@gmail.com>
2023-06-29 22:15:51 -0300

    funcao captura de imagem

--- monibank-main/JS/camera.js
+++ monibank-main/JS/camera.js
@@ -2,6 +2,10 @@ const botaoIniciarCamera = document.querySelector("[data-video-botao]");
 const campoCamera = document.querySelector("[data-camera]");
 const video = document.querySelector("[data-video]");
 const botaoTirarFoto = document.querySelector("[data-tirar-foto]");
+const canvas = document.querySelector("[data-video-canvas]");
+const mensagem = document.querySelector("[data-mensagem]");
+
+let imageURL = "";
 
 botaoIniciarCamera.addEventListener("click", async function () {
    const iniciarVideo = await navigator.mediaDevices.getUserMedia({
@@ -12,4 +16,11 @@ botaoIniciarCamera.addEventListener("click", async function () {
    campoCamera.style.display = "block";
 
    video.srcObject = iniciarVideo;
+});
+
+botaoTirarFoto.addEventListener("click", function () {
+   canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+   imageURL = canvas.toDataURL("image/jpeg");
+   campoCamera.style.display = "none";
+   mensagem.style.display = "block";
 });
